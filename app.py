1f40457e350bb4eaafed4f0a1638b27a771c40e1
Oohoo261 <64023689@up.ac.th>
2024-08-30 12:36:20 +0700

    update file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import torch
 app = Flask(__name__, template_folder="templates")
 
 # Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp2/weights/best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 
 classNames = ['Atlas-moth', 'Black-Grass-Caterpillar', 'Coconut-black-headed-caterpillar', 'Common cutworm', 'Cricket', 'Diamondback-moth',
          'Fall-Armyworm', 'Grasshopper', 'Green-weevil', 'Leaf-eating-caterpillar', 'Oriental-Mole-Cricket', 'Oriental-fruit-fly',
